a9fb2325600f72faad60a50e3fe0f5beb74c3297
DangerCow <38820244+DangerCow@users.noreply.github.com>
2019-08-15 17:00:00 -0700

    version 1.0

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace dcs_game_launcher_prototype_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gamesList = new System.Windows.Forms.CheckedListBox();
+            this.play_btn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // gamesList
+            // 
+            this.gamesList.CheckOnClick = true;
+            this.gamesList.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamesList.FormattingEnabled = true;
+            this.gamesList.HorizontalScrollbar = true;
+            this.gamesList.Location = new System.Drawing.Point(12, 12);
+            this.gamesList.Name = "gamesList";
+            this.gamesList.Size = new System.Drawing.Size(284, 400);
+            this.gamesList.TabIndex = 0;
+            this.gamesList.UseCompatibleTextRendering = true;
+            this.gamesList.SelectedValueChanged += new System.EventHandler(this.gamesList_SelectedValueChanged);
+            // 
+            // play_btn
+            // 
+            this.play_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.play_btn.Location = new System.Drawing.Point(12, 418);
+            this.play_btn.Name = "play_btn";
+            this.play_btn.Size = new System.Drawing.Size(284, 126);
+            this.play_btn.TabIndex = 1;
+            this.play_btn.Text = "play";
+            this.play_btn.UseVisualStyleBackColor = true;
+            this.play_btn.Click += new System.EventHandler(this.play_btn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(302, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(599, 516);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 556);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.play_btn);
+            this.Controls.Add(this.gamesList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "dcs game launcher 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox gamesList;
+        private System.Windows.Forms.Button play_btn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dcs_game_launcher_prototype_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        public Dictionary<string, string> games = new Dictionary<string, string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.Title = "dcs game launcher console";
+            }
+            catch { }
+            log("loaded game launcher");
+
+            if (!File.Exists("data.txt") || !Directory.Exists("data"))
+            {
+                if (!File.Exists("data.txt")) { File.Create("data.txt"); }
+                if (!Directory.Exists("data")) { Directory.CreateDirectory("data"); }
+                System.Windows.Forms.Application.Restart();
+            }
+
+
+            WebClient client = new WebClient();
+            string games_data = client.DownloadString("https://coding-prototype-database.glitch.me/data3.txt");
+
+            foreach (string line in games_data.Split('\n'))
+            {
+                log("loaded game: " + line);
+                string key = line.Split('|')[0];
+                string value = line.Split('|')[1];
+
+                gamesList.Items.Add(key);
+
+                games.Add(key, value);
+            }
+        }
+
+        private void gamesList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            gamesList.SelectedIndex = -1;
+        }
+
+        private void play_btn_Click(object sender, EventArgs e)
+        {
+            if(gamesList.CheckedItems.Count == 1){
+                log("\nstarting: " + gamesList.CheckedItems[0] + "\n");
+                start_game(games[gamesList.CheckedItems[0].ToString()], gamesList.CheckedItems[0].ToString());
+            }
+            else if(gamesList.CheckedItems.Count == 0)
+            {
+                log("no game selected");
+            }
+            else
+            {
+                log("cannot slect more then 1 game");
+            }
+        }
+
+        private void start_game(string url, string name)
+        {
+            log("downloading game config: " + url);
+
+            WebClient client = new WebClient();
+            string data = client.DownloadString(url);
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(data);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            log("geting varibles from config");
+            //get varibles from game data
+            string[] lines = data.Split('\n');
+
+            string version = lines[1];
+            string zip_link = lines[4];
+            string zip_directroy = lines[7];
+            string unzip_directroy = lines[10];
+            string game_directroy = lines[13];
+
+            bool game_in_data = false;
+            int old_ver_line = 0;
+            int i = 0;
+
+            string old_ver = "";
+
+            List<string> data_lines = File.ReadAllLines("data.txt").ToList();
+            foreach (string line in data_lines)
+            {
+                if (line.Split('|')[0] == name) { game_in_data = true;  old_ver_line = i; }
+                i++;
+            }
+
+            if (!game_in_data)
+            {
+                data_lines.Add(name + "|" + version);
+                //download game
+                log("downloading game: " + game_directroy);
+                data_lines.Add(name + "|" + version);
+                download_then_run_game(version, zip_link, zip_directroy, unzip_directroy, game_directroy);
+            }
+            else
+            {
+                old_ver = data_lines[old_ver_line].Split('|')[1] ;
+                if (old_ver != version)
+                {
+                    //download game
+                    log("downloading update: " + game_directroy);
+                    data_lines[old_ver_line] = name + "|" + version;
+                    download_then_run_game(version, zip_link, zip_directroy, unzip_directroy, game_directroy);
+                }
+                else
+                {
+                    //run game
+                    run_game(game_directroy);
+                }
+            }
+
+            System.IO.File.WriteAllLines("data.txt", data_lines);
+        }
+
+        private void download_then_run_game(string version, string zip_link, string zip_directroy, string unzip_directory, string game_directory)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(zip_link, zip_directroy);
+
+            try
+            {
+                var dir = new DirectoryInfo(unzip_directory);
+                dir.Attributes = dir.Attributes & ~FileAttributes.ReadOnly;
+                dir.Delete(true);
+            }
+            catch { }
+
+            ZipFile.ExtractToDirectory(zip_directroy, unzip_directory);
+
+            run_game(game_directory);
+        }
+
+        private void run_game(string game_directory)
+        {
+            log("runing game");
+
+            string sub_directroy = string.Join("/", game_directory.Split('/').Take(game_directory.Split('/').Count() - 1));
+            string exe_name = game_directory.Split('/')[game_directory.Split('/').Count() - 1];
+
+            System.Diagnostics.Process.Start("CMD.exe", "/K " + "cd " + sub_directroy + "&&" + exe_name);
+        }
+
+        private void log(string log)
+        {
+            Console.WriteLine(log);
+            listBox1.Items.Add(log);
+        }
+    }
+}
